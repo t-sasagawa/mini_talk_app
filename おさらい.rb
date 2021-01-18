7e384cee2b84d24a70edf40e621403c3dbb6f14a
t-sasagawa <74903855+t-sasagawa@users.noreply.github.com>
2021-01-18 17:15:55 +0900

    作成方法

--- おさらい.rb
+++ おさらい.rb
@@ -51,3 +51,41 @@
 # 2 → config/environment/production.rb ファイルの編集
 # Herokuへデプロイする場合には、commit と pushを行い、git push heroku masterコマンドにてデプロイを行う。
 # 「FURIMAへの追加実装ではなく、ミニアプリをデプロイされる場合は、"予めローカルリポジトリが作成されていることが条件"となる。」
+
+# ----------
+
+# Heroku上にアプリケーションを作成
+# heroku create herokuのアプリ名-数字
+
+# 設定できたことを確認
+# git config --list | grep heroku
+
+# Heroku上でMySQLを使えるようにする
+# Herokuでは、使用するデータベースの設定が、デフォルトでPostgreSQLというデータベースになっている。
+# ClearDBというデータベースサービスが提供しているアドオンで、これを追加することにより、HerokuでMySQLを使用できるようになる。
+# heroku addons:add cleardb
+
+# ClearDBデータベースのURLを変数heroku_cleardbに格納する。
+# heroku_cleardb=`heroku config:get CLEARDB_DATABASE_URL`
+
+# データベースのURLを再設定する。
+# デフォルトでmysql2というGemを使用しているので、DATABASE_URLの冒頭がmysql2://に変更されていることも確認する。
+# heroku config:set DATABASE_URL=mysql2${heroku_cleardb:5}
+
+# Heroku上にマスターキーを設置する
+# heroku config:set RAILS_MASTER_KEY=`cat config/master.key`
+
+# heroku config
+# マスターキーが設定されているか確認
+
+# herokuで使用するサービスのバージョン指定を行う
+# heroku stack:set heroku-18 -a herokuのアプリ名-数字
+
+# herokuへアプリを追加
+# git push heroku master
+
+# herokuへデータ反映
+# heroku run rails db:migrate
+
+# 情報確認
+# heroku apps:info
